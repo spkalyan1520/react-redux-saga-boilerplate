086d403874eff0be0773cee6e8f337dc71a80f16
Kalyan Sripathi <k0s01x7@m-c02yw1rclvdt.hsd1.ca.comcast.net>
2019-08-19 00:37:07 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Minimal React, Redux, Redux Saga boilerplate all wired up. For specific commands please refer to create react app documentation
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
---
+++ src/sagas.js
@@ -0,0 +1,29 @@
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import { REQUEST_HELLO_WORLD, RECEIVE_HELLO_WORLD, receiveHelloWorld } from './actions';
+
+// worker Saga: will be fired on USER_FETCH_REQUESTED actions
+function* helloWorld(action) {
+   try {
+       //do api call
+      //const user = yield call(Api.fetchUser, action.payload.userId);
+      yield put(receiveHelloWorld("Hello World from redux saga!"));
+   } catch (e) {
+      yield put({
+          type: "USER_FETCH_FAILED", 
+          message: e.message
+        });
+   }
+}
+
+/*
+  Alternatively you may use takeLatest.
+
+  Does not allow concurrent fetches of user. If "USER_FETCH_REQUESTED" gets
+  dispatched while a fetch is already pending, that pending fetch is cancelled
+  and only the latest one will be run.
+*/
+function* mySaga() {
+  yield takeLatest(REQUEST_HELLO_WORLD, helloWorld);
+}
+
+export default mySaga;
